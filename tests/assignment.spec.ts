cde41329d1e938faa2c0236900a48e220a202924
cacelajado <carol.acelajado@gmail.com>
2024-05-10 10:56:50 +0100

    with running assignment

--- tests/assignment.spec.ts
+++ tests/assignment.spec.ts
@@ -4,10 +4,9 @@ test('Search for "Football" on BBC Sport', async ({ page }) => {
   // Step 1: Navigate to https://www.bbc.co.uk/sport
   await page.goto('https://www.bbc.co.uk/sport');
   
-
+/*
   // Step 2: Locate the search field
-  const searchField = await page.locator('input[name="q"]');
-  test.setTimeout(150_000); // Set a longer timeout (e.g., 150 seconds)
+  const searchField = await page.getByLabel('Search BBC');
   await searchField.fill('Football');
 
   // Step 3: Click the Search button
@@ -17,4 +16,25 @@ test('Search for "Football" on BBC Sport', async ({ page }) => {
   // Step 4: Verify results
   const results = await page.locator('.search-results');
   await expect(results).toHaveText('Football');
+
+  */
+
+  // Step 2: Locate the search field in header
+  const searchFieldHeader = await page.getByLabel('Search BBC');
+  await searchFieldHeader.click();
+
+  //Step 3: Enter search term
+  const searchTerm = await page.getByPlaceholder('Search the BBC');
+  await searchTerm.fill('Football');
+
+  // Step 4: Click the Search button
+    const searchButton = await page.locator('button[type="submit"]');
+    await searchButton.click();
+
+
+  // Step 5: Validate results
+  const filteredResults = await page.locator('//ul[@role="list"]/li//span').filter({ hasText: 'Football' });
+  const footballResult = await filteredResults.first();
+  await expect(footballResult).toContainText('Football');
+        
 });
